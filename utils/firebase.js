3ae690821821fdacc890f2dcd0432062f201d8c9
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-06-15 18:35:48 +0530

    firebase env var set

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,9 +1,14 @@
-const Firebase = require('firebase-admin')
-const serviceAccount = require('../drive-28da5-firebase-adminsdk-fbsvc-972d77b40c.json')
+require('dotenv').config(); // load .env variables
+
+const Firebase = require('firebase-admin');
 
 const firebase = Firebase.initializeApp({
-    credential: Firebase.credential.cert(serviceAccount),
-    storageBucket: 'drive-28da5.firebasestorage.app'
-})
+    credential: Firebase.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
 
-module.exports = Firebase
+module.exports = Firebase;
