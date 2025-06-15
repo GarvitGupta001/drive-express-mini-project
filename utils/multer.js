59c72369214c9516272d86cc609a80fcb495c8c9
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-06-15 18:52:17 +0530

    update multer.js

--- utils/multer.js
+++ utils/multer.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const multer = require('multer')
 const firebaseStorage = require('multer-firebase-storage')
 const firebase = require('./firebase')
 const serviceAccount = require('../drive-28da5-firebase-adminsdk-fbsvc-972d77b40c.json')
 
 const storage = firebaseStorage({
-    credentials: firebase.credential.cert(serviceAccount),
-    bucketName: 'drive-28da5.firebasestorage.app',
+    credentials: firebase.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    bucketName: process.env.FIREBASE_STORAGE_BUCKET,
     unique:true,
 })
 
